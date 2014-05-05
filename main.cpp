e32c5198a1aae0d88ef70c6a635b1619520423b5
Christof Sigel <christof.sigel@internode.on.net>
2014-05-05 10:18:59 +0930

    use opengl 3.2 instead of 4.0

--- main.cpp
+++ main.cpp
@@ -154,8 +154,8 @@ int main(int argc, char **argv)
 	return 1;
     }
 
-    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 4 );
-    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 0 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 2 );
 
     SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 8 );
     SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 8 );
