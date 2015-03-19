4c749eaf08a7477f144ebe3fdbe3c033cf3d1a0a
Christof Sigel <christof.sigel@internode.on.net>
2015-03-19 14:24:43 +1030

    use stb_truetype to render text?

--- main.cpp
+++ main.cpp
@@ -54,6 +54,43 @@ GLuint RenderColorLocation;
 extern uint8_t TAPalette[256*3];
 GLfloat TAPaletteF[256*3];
 
+#define STB_TRUETYPE_IMPLEMENTATION  // force following include to generate implementation
+#include "stb_truetype.h"
+char ttf_buffer[1<<20];
+unsigned char temp_bitmap[512*512];
+stbtt_bakedchar cdata[96]; // ASCII 32..126 is 95 glyphs
+GLstbtt_uint ftex;
+void my_stbtt_initfont(void)
+{
+   fread(ttf_buffer, 1, 1<<20, fopen("data/times.ttf", "rb"));
+   stbtt_BakeFontBitmap(data,0, 32.0, temp_bitmap,512,512, 32,96, cdata); // no guarantee this fits!
+   // can free ttf_buffer at this point
+   glGenTextures(1, &ftex);
+   glBindTexture(GL_TEXTURE_2D, ftex);
+   glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, 512,512, 0, GL_ALPHA, GL_UNSIGNED_BYTE, temp_bitmap);
+   // can free temp_bitmap at this point
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+}
+
+void my_stbtt_print(float x, float y, char *text)
+{
+   // assume orthographic projection with units = screen pixels, origin at top left
+   glBindTexture(GL_TEXTURE_2D, ftex);
+   glBegin(GL_QUADS);
+   while (*text) {
+      if (*text >= 32 && *text < 128) {
+         stbtt_aligned_quad q;
+         stbtt_GetBakedQuad(cdata, 512,512, *text-32, &x,&y,&q,1);//1=opengl & d3d10+,0=d3d9
+         glTexCoord2f(q.s0,q.t1); glVertex2f(q.x0,q.y0);
+         glTexCoord2f(q.s1,q.t1); glVertex2f(q.x1,q.y0);
+         glTexCoord2f(q.s1,q.t0); glVertex2f(q.x1,q.y1);
+         glTexCoord2f(q.s0,q.t0); glVertex2f(q.x0,q.y1);
+      }
+      ++text;
+   }
+   glEnd();
+}
+
 void render()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -84,7 +121,7 @@ void render()
     
     //TempSphere->RotateX(30.0f/60.0f);
     //TempSphere->Render();
-
+    my_stbtt_print(0,0,"this is a TEST");
     
 }
 
@@ -114,7 +151,7 @@ void LoadCurrent3doFile()
 
 void setup()
 {
-
+    my_stbtt_initfont();
     hpi=new HPI("data/totala1.hpi");
     LoadTextureList();
     
