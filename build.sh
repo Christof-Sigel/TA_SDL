2e987f2a1713db65c008cba30b651d1aa4a18b2d
Christof Sigel <christof.sigel@internode.on.net>
2015-03-21 21:45:32 +1030

    Ignore some warnings.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-clang++ -lSDL2 -ggdb -Wall -lGLEW -lGLU -lGL ta_sdl.cpp -o ta_sdl
+#!/bin/bash
+IGNORE_WARNINGS=-Wno-c++11-extensions
+clang++ -lSDL2 -ggdb -Wall -lGLEW -lGLU -lGL ta_sdl.cpp $IGNORE_WARNINGS -o ta_sdl
 
 
