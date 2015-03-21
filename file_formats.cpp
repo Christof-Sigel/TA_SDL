e16776c478a9dcac9ffdf1e64d56ba3d4bbcfe99
Christof Sigel <christof.sigel@internode.on.net>
2015-03-22 08:36:16 +1030

    New style HPI File Loading.

--- file_formats.cpp
+++ file_formats.cpp
@@ -44,6 +44,8 @@ struct HPIFile
 
 const int32_t SAVE_MARKER= 'B' << 0 | 'A' <<8 | 'N' << 16 | 'K' <<24;
 const int32_t HPI_MARKER= 'H' << 0 | 'A' <<8 | 'P' <<16 | 'I' << 24;
+const int32_t CHUNK_MARKER = 'S' << 0 | 'Q' <<8 | 'S' <<16 | 'H' << 24;
+const int32_t CHUNK_SIZE = 65536;
 
 struct FILE_HPIHeader
 {
@@ -74,9 +76,22 @@ struct __attribute__((__packed__)) FILE_HPIFileData
     char Flag;// 0 -> File,  1 -> Directory
 };
 
-void DecryptHPIBuffer(char * Source, char * Destination, int32_t Length, int32_t DecryptionKey, int32_t FileOffset);
+struct __attribute__((__packed__)) FILE_HPIChunk
+{
+    int32_t Marker;
+    char Unknown1;
+    char CompressionMethod;
+    char Encrypt;
+    int32_t CompressedSize;
+    int32_t DecompressedSize;
+    int32_t Checksum;
+};
+void DecryptHPIBuffer(HPIFile * HPI, char * Destination, int32_t Length, int32_t FileOffset);
 void LoadEntries(HPIDirectoryEntry * Root, char * Buffer, int Offset, HPIFile * File);
-
+char * LoadChunk(char * SourceBuffer, char * Destination);
+void DecompressZLibChunk(char * Source, int CompressedSize, int DecompressedSize, char * Destination);
+void DecompressLZ77Chunk(char * Source, int CompressedSize, int DecompressedSize, char * Destination);
+    
 bool32 LoadHPIFile(const char * FileName, HPIFile * HPI)
 {
     MemoryMappedFile HPIMemory = MemoryMapFile(FileName);
@@ -102,7 +117,7 @@ bool32 LoadHPIFile(const char * FileName, HPIFile * HPI)
 
     //TODO(Christof): Figure out a memory alloc scheme, just use malloc for now
     char * DecryptedDirectory = (char *)malloc(header->DirectorySize);
-    DecryptHPIBuffer((char*)HPIMemory.MMapBuffer,DecryptedDirectory,header->DirectorySize, HPI->DecryptionKey, 0);
+    DecryptHPIBuffer(HPI,DecryptedDirectory,header->DirectorySize, 0);
     LoadEntries(&HPI->Root, DecryptedDirectory, header->Offset,HPI);
 
     return 1;
@@ -137,13 +152,13 @@ void LoadEntries(HPIDirectoryEntry * Root, char * Buffer, int Offset,HPIFile * F
     }
 }
 
-void DecryptHPIBuffer(char * Source, char * Destination, int32_t Length, int32_t DecryptionKey, int32_t FileOffset)
+void DecryptHPIBuffer(HPIFile * HPI, char * Destination, int32_t Length, int32_t FileOffset)
 {
     int CurrentKey=0;
     for(int BufferIndex=0;BufferIndex < Length; BufferIndex++)
     {
-	CurrentKey =  (BufferIndex + FileOffset) ^ DecryptionKey;
-	Destination[BufferIndex] = CurrentKey ^ ~Source[BufferIndex+FileOffset];
+	CurrentKey =  (BufferIndex + FileOffset) ^ HPI->DecryptionKey;
+	Destination[BufferIndex] = CurrentKey ^ ~ (HPI->MMFile.MMapBuffer[BufferIndex+FileOffset]);
     }
 }
 
@@ -157,17 +172,135 @@ bool32 LoadHPIFileEntryData(HPIEntry Entry, char * Destination)
     switch(Entry.File.Compression)
     {
     case COMPRESSION_NONE:
-	DecryptHPIBuffer((char*)Entry.ContainedInFile->MMFile.MMapBuffer,Destination,Entry.File.FileSize,Entry.ContainedInFile->DecryptionKey,Entry.File.Offset);
+	DecryptHPIBuffer(Entry.ContainedInFile,Destination,Entry.File.FileSize,Entry.File.Offset);
 	return 1;
     case COMPRESSION_LZ77:
-	printf("%s is compressed using LZ77 - this is not currently implemented\n",Entry.Name);
-	return 0;
     case COMPRESSION_ZLIB:
-	printf("%s is compressed using zlib - this is not currently implemented\n",Entry.Name);
-	return 0;
+	//not entirely sure what the point of this is since each chunk also has a compression metho flag
+    {
+	int NumChunks = Entry.File.FileSize / CHUNK_SIZE;
+	if(Entry.File.FileSize % CHUNK_SIZE)
+	{
+	    NumChunks++;
+	}
+	int32_t ChunkSizes[NumChunks];
+	DecryptHPIBuffer(Entry.ContainedInFile,(char*)ChunkSizes,NumChunks*sizeof(int32_t),Entry.File.Offset);
+	int ChunkDataSize=NumChunks*sizeof(FILE_HPIChunk);//size of all the headers
+	int ChunkDataOffset = Entry.File.Offset + NumChunks*sizeof(int32_t);
+	for(int i=0;i<NumChunks;i++)
+	{
+	    ChunkDataSize += ChunkSizes[i];
+	}
+	char DecryptedChunkData[ChunkDataSize];
+	DecryptHPIBuffer(Entry.ContainedInFile,DecryptedChunkData,ChunkDataSize,ChunkDataOffset);
+	char * DataSource=DecryptedChunkData;
+	for(int i=0;i<NumChunks;i++)
+	{
+	    if(!DataSource)
+	    {
+		//something broke
+		return 0;
+	    }
+	    DataSource=LoadChunk(DataSource,&Destination[i*CHUNK_SIZE]);
+	}
+    }
     }
 
     return 1;
+}
+
+char * LoadChunk(char * Source, char * Destination)
+{
+    FILE_HPIChunk * header=(FILE_HPIChunk *)Source;
+    if(header->Marker != CHUNK_MARKER)
+    {
+	printf("chunk with incorrect marker %d\n",header->Marker);
+	return 0;
+    }
+    char * data=Source+sizeof(FILE_HPIChunk);
+    int32_t ComputedChecksum=0;
+    for(int i=0;i<header->CompressedSize;i++)
+    {
+	ComputedChecksum += (unsigned char)data[i];
+    }
+    if(ComputedChecksum != header->Checksum)
+    {
+     	printf("Chunk Checksums do not match: %d != %d\n",ComputedChecksum,header->Checksum);
+     	return 0;
+    }
+    if(header->Encrypt)
+    {
+	for(int i=0;i<header->CompressedSize;i++)
+	    data[i]=(data[i] - i) ^ i;
+    }
+    Compression_Type CompressionMethod= (Compression_Type)header->CompressionMethod;
+    switch(CompressionMethod)
+    {
+    case COMPRESSION_ZLIB:
+	DecompressZLibChunk(data,header->CompressedSize,header->DecompressedSize,Destination);
+	break;
+    case COMPRESSION_LZ77:
+	DecompressLZ77Chunk(data,header->CompressedSize,header->DecompressedSize,Destination);
+	break;
+    default:
+	printf("Unknown Compression method %d in chunk\n",CompressionMethod);
+	break;
+    }
+    
+    return data + header->CompressedSize;
+}
+
+void DecompressZLibChunk(char * Source, int CompressedSize, int DecompressedSize, char * Destination)
+{
+    printf("Zlib compression not yet supported\n");
+}
+
+
+const int LZ77_WINDOW_SIZE=4096;
+const int LZ77_LENGTH_MASK=0x0f;
+void DecompressLZ77Chunk(char * Source, int CompressedSize, int DecompressedSize, char * Destination)
+{
+    unsigned char Window[LZ77_WINDOW_SIZE];
+    
+    int windowIndex=1;
+    while(true)
+    {
+	int tag=*Source++;
+	int mask=1;
+	for(int MaskCount=0;MaskCount<8;MaskCount++,mask*=2)
+	{
+	    if((tag & mask) == 0)
+	    {
+		//read a byte, put it in window and buffer
+		*Destination++=*Source;
+		Window[windowIndex++]=*Source++;
+		if(windowIndex>=LZ77_WINDOW_SIZE)
+		    windowIndex=0;
+	    }
+	    else
+	    {
+		//read two bytes, lower 4 bytes are length, upper 12 are offset into window
+		
+		int offset=(*(uint16_t*)Source)>>4;
+		int length=(*(uint16_t*)Source)&LZ77_LENGTH_MASK;
+		length+=2;
+		if(offset==0)
+		{
+		    return;
+		}
+		Source+=2;
+		for(int readCount=0;readCount<length;readCount++)
+		{
+		    *Destination++=Window[offset];
+		    Window[windowIndex++]=Window[offset++];
+		    if(offset>=LZ77_WINDOW_SIZE)
+			offset=0;
+		    if(windowIndex>=LZ77_WINDOW_SIZE)
+			windowIndex=0;
+		}
+	    }
+	}
+    }
 }
 
 HPIEntry FindHPIEntry(HPIDirectoryEntry Directory, char * Path)
