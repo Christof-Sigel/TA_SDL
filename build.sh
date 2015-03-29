03594b233ea30c5f0f15785959318fb8245eab9f
Christof Sigel <christof.sigel@internode.on.net>
2015-03-30 06:15:56 +1030

    compiler output in color for server build script.

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 IGNORE_WARNINGS=-Wno-c++11-extensions
-clang++ -lSDL2 -ggdb -Wall -lGLEW -lGLU -lGL -lz -lpng ta_sdl.cpp $IGNORE_WARNINGS -D__CSGL_DEBUG__ -o ta_sdl
-
-
+clang++ -lSDL2 -ggdb -Wall -lGLEW -lGLU -lGL -lz -lpng ta_sdl.cpp $IGNORE_WARNINGS -D__CSGL_DEBUG__ -Xclang -fcolor-diagnostics -o ta_sdl
