628af530ab6f41634e11f0ebe60152748cd9ac77
Christof Sigel <christof.sigel@internode.on.net>
2015-06-06 21:22:36 +0930

    better output filenames for windows build

--- build.bat
+++ build.bat
@@ -11,8 +11,8 @@ REM 64-bit build
 del *.pdb > NUL 2> NUL
 REM Optimization switches /O2
 echo WAITING FOR PDB > lock.tmp
-cl %CommonCompilerFlags%  ..\ta_sdl_game.cpp ..\Logging.cpp -Fmta_sdl_game.map -LD /link -incremental:no -opt:ref -PDB:handmade_%random%.pdb -EXPORT:GameSetup -EXPORT:CheckResources -EXPORT:GameTeardown -EXPORT:GameUpdateAndRender -libpath:../w64/lib OpenGL32.lib Glu32.lib libz.a glew32.lib SDL2.lib
+cl %CommonCompilerFlags%  ..\ta_sdl_game.cpp ..\Logging.cpp -Fmta_sdl_game.map -LD -Feta_sdl.dll /link -incremental:no -opt:ref -PDB:ta_sdl_dll_%random%.pdb -EXPORT:GameSetup -EXPORT:CheckResources -EXPORT:GameTeardown -EXPORT:GameUpdateAndRender -libpath:../w64/lib OpenGL32.lib Glu32.lib libz.a glew32.lib SDL2.lib
 del lock.tmp
-cl %CommonCompilerFlags% ..\ta_sdl_platform.cpp ..\Logging.cpp -Fmta_sdl_platform.map -DDLL_NAME=ta_sdl_game.dll -DDLL_TEMP_NAME=ta_sdl_game_temp.dll -Feta_sdl.exe  /link -libpath:../w64/lib OpenGL32.lib Glu32.lib glew32.lib SDL2.lib %CommonLinkerFlags% -subsystem:console
+cl %CommonCompilerFlags% ..\ta_sdl_platform.cpp ..\Logging.cpp -Fmta_sdl_platform.map -DDLL_NAME=ta_sdl.dll -DDLL_TEMP_NAME=ta_sdl_temp.dll -Feta_sdl.exe  /link -libpath:../w64/lib OpenGL32.lib Glu32.lib glew32.lib SDL2.lib %CommonLinkerFlags% -subsystem:console
 
 popd
