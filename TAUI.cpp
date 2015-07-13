991b54ecd8084179a78cce4361bbfab14030ab54
Christof Sigel <christof.sigel@internode.on.net>
2015-07-13 16:37:28 +0930

    TAUI struct skeleton.

--- TAUI.cpp
+++ TAUI.cpp
@@ -1,18 +1,73 @@
-enum FixedMenuItem
+enum TagType
 {
-    FIXED_MENU_ITEM_NONE,
-    FIXED_MENU_ITEM_SINGLE
-    
+    TAG_UI_CONTAINER =0,
+    TAG_BUTTON,
+    TAG_LISTBOX,//No extra details
+    TAG_TEXTFIELD,
+    TAG_SCROLLBAR,
+    TAG_LABEL,
+    TAG_DYNAMIC_IMAGE,
+    TAG_LABEL_FONT,
+    TAG_IMAGE = 12//No extra details
 };
 
+
+#define TAUI_ATTRIBUTE_SCROLLBAR_HORIZONTAL 1
+#define TAUI_ATTRIBUTE_SCROLLBAR_VERTICAL 2
 
 struct TAUIElement
 {
     char * Name;
-    int ID, Association,X,Y,Width,Height,Attributes,ColorFore,ColorBack,TextureNumber,FontNumber;
-    FixedMenuItem Item;
-    
+    TagType ElementType;
+    int Association,X,Y,Width,Height,ColorFore,ColorBack,TextureNumber,FontNumber;
+    int Attributes, CommonAttributes;
+    char * Help;
+    uint8_t Visible;//Pulls from active
+    void * Details;
 };
+
+struct TAUIContainer
+{
+    Texture * Background;//Name is in Panel
+    TAUIElement * DefaultFocus;
+};
+
+struct TAUIButton
+{
+    int StartingFrame;//pulls from status
+    int Stages;
+    char * Text;// | seperator for multistage buttons, enter aligned for simple (single stage) buttons, right aligned otherwise
+    uint8_t Disabled;//pulls from grayedout
+};
+
+struct TAUITextBox
+{
+    int MaximumCharacters;
+};
+
+struct TAUIScrollbar
+{
+    int Maximum;
+    int Position;
+    int KnobSize;
+};
+
+struct TAUILabel
+{
+    char * Text;
+    TAUIButton * Link;
+};
+
+struct TAUIDynamicImage
+{
+    uint8_t DisaplySelectionRectangle;//Puller from hotornot
+};
+
+struct TAULabelFont
+{
+    FNTFont * Font;//from filename
+};
+
 
 
 TAUIElement LoadGUIFromBuffer(char * Buffer)
